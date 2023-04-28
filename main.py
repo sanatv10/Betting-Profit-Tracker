1c7a9b5438da6cc5a432a76c78326b13725d4ea0
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2023-04-28 13:33:41 -0700

    Added login window

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 
+def login_window():
+    window = Tk()
+    window.geometry("300x250")
+    window.title("Login")
+
+    user_label = Label(window, text="Username:")
+    user_label.pack()
+    user_field = Entry(window)
+    user_field.pack()
+    password_label = Label(window, text="Username:")
+    password_label.pack()
+    password_field = Entry(window)
+    password_field.pack()
+
+    login_button = Button(window, text="Login", command=login)
+    login_button.pack()
+
+    register_button = Button(window, text="Login", command=login)
+    register_button.pack()
+
+    window.mainloop()
+
+
+def login():
+    print("login success")
+
+
+login_window()
