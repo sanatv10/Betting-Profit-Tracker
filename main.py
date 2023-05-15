a7ce8a5e3e0e5e1c40938a26f03e058a0ec7475f
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2023-05-14 23:37:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,6 @@ current_user = ""
 #
 #    conn.close()
 
-
-def create_users_table():
-    conn = sqlite3.connect("users.db")
-    c = conn.cursor()
-
-    c.execute("""
-        CREATE TABLE users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
-            );
-    """)
-
-    conn.commit()
-    conn.close()
-
-
 def drop_users_table():
     conn = sqlite3.connect("users.db")
     c = conn.cursor()
